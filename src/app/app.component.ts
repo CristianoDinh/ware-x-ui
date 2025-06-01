0e60412a990c28de5037eb6f8f7beff4569bff26
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-06-01 23:52:07 +0700

    create user-auth component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {UserAuthComponent} from './components/user-auth/user-auth.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserAuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {UserAuthComponent} from './components/user-auth/user-auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'account',
+    component: UserAuthComponent,
+    title: 'Account'
+  }
+];
