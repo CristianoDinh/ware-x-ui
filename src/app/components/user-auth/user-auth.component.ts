bc816c6e383dfa21a88010d8d1f4f4ba4ddbb870
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-06-02 09:31:51 +0700

    update ui

--- src/app/components/user-auth/user-auth.component.ts
+++ src/app/components/user-auth/user-auth.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {NgClass, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-user-auth',
-  imports: [],
+  imports: [
+    NgClass,
+    NgIf
+  ],
   templateUrl: './user-auth.component.html',
   styleUrl: './user-auth.component.css'
 })
 export class UserAuthComponent {
+  isLogin: boolean = true;
 
+  toggleForm() {
+    this.isLogin = !this.isLogin;
+  }
 }
