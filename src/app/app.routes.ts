fe8f97680850ba8b289bccf631d0d6871de2bc33
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-06-02 12:45:46 +0700

    create component for in-progress features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import {UserAuthComponent} from './components/user-auth/user-auth.component';
 import {HomeComponent} from './components/home/home.component';
+import {ComingSoonComponent} from './components/coming-soon/coming-soon.component';
 
 export const routes: Routes = [
   { path: '',
     redirectTo: 'auth',
     pathMatch: 'full'
   },
+
   {
     path: 'auth',
     component: UserAuthComponent,
@@ -16,6 +18,15 @@ export const routes: Routes = [
     path: 'home',
     component: HomeComponent,
     title: 'HomePage'
+  },
+  {
+    path: 'coming-soon',
+    component: ComingSoonComponent,
+    title: ''
+  },
+  { path: '**',
+    redirectTo: 'auth',
+    pathMatch: 'full'
   },
 
 ];
