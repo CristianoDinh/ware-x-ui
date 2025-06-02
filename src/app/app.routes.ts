c1396e1da6fdc99ff8b74cee0d13d7aa31a4dd09
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-06-02 11:39:45 +0700

    create homepage and config route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
 import {UserAuthComponent} from './components/user-auth/user-auth.component';
+import {HomeComponent} from './components/home/home.component';
 
 export const routes: Routes = [
+  { path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
   {
-    path: 'account',
+    path: 'auth',
     component: UserAuthComponent,
     title: 'Account'
-  }
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: 'HomePage'
+  },
+
 ];
